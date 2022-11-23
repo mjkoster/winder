5069c36a1d42df117bc009fb315f2a2b5fa8b692
mjkoster <michaeljohnkoster@gmail.com>
2022-11-23 14:35:52 -0800

    Update encoderSetter.py
    refactor

--- encoderSetter.py
+++ encoderSetter.py
@@ -4,7 +4,7 @@ import rotaryio
 import TM1637
 import time
 
-class valueSetter():
+class valueEncoder():
     def __init__ (self, clk, dio, enca, encb, butt, minValue=0, maxValue=100, precision=1, ratio=1 ,pratio=0, vinit=None):
         self.display = TM1637.TM1637Decimal(clk, dio)
         self.encoder = rotaryio.IncrementalEncoder(enca, encb)
@@ -20,31 +20,37 @@ class valueSetter():
         else:
             self.width = 5
         if vinit != None:
-            self.value = vinit
+            self._value = vinit
         else:
-            self.value = self.minValue
+            self._value = self.minValue
         self.lastPosition = 0
         self.display.write(self.display.encode_string("8.8.8.8."))
         time.sleep(1)
-        self.display.write(self.display.encode_string("{:{w}.{p}f}".format(self.value, w = self.width, p = self.precision)))
+        self.display.write(self.display.encode_string("{:{w}.{p}f}".format(self._value, w = self.width, p = self.precision)))
 
     def poll(self):
         self.position = self.encoder.position
         if self.position != self.lastPosition:
-            if self.button.value:
-                self.value += (self.position - self.lastPosition) * 10**-self.precision * 10**self.ratio
+            if self.button._value:
+                self._value += (self.position - self.lastPosition) * 10**-self.precision * 10**self.ratio
             else:
-                self.value += (self.position - self.lastPosition) * 10**-self.precision * 10**self.pratio
-            self.value = min(self.value, self.maxValue)
-            self.value = max(self.value, self.minValue)
-            self.value = round(self.value,self.precision)
-            self.display.write(self.display.encode_string("{:{w}.{p}f}".format(self.value, w = self.width, p = self.precision)))
+                self._value += (self.position - self.lastPosition) * 10**-self.precision * 10**self.pratio
+            self._value = min(self._value, self.maxValue)
+            self._value = max(self._value, self.minValue)
+            self._value = round(self._value,self.precision)
+            self.display.write(self.display.encode_string("{:{w}.{p}f}".format(self._value, w = self.width, p = self.precision)))
             self.lastPosition = self.position
             return True
         return False
 
-    def currentValue(self):
-        return self.value
+    @property
+    def value(self):
+        return self._value
+
+    @value.setter
+    def value(self, setValue):
+        self._value = setValue
+
 
 if __name__ == '__main__':
     clockPin = board.D51
@@ -52,9 +58,9 @@ if __name__ == '__main__':
     encoderaPin = board.D25
     encoderbPin = board.D26
     encoderButtonPin = board.D27
-    testSetter = valueSetter(clockPin, dioPin, encoderaPin, encoderbPin, encoderButtonPin)
+    testSetter = valueEncoder(clockPin, dioPin, encoderaPin, encoderbPin, encoderButtonPin)
 
     while True:
         if testSetter.poll():
-            print(testSetter.currentValue())
+            print(testSetter.value())
         time.sleep(0.1)
